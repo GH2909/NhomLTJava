58fcbbf0dbc8e055f977e2ece86df3404e26ba9f
vantien68 <tienlv8664@ut.edu.vn>
2025-03-25 02:25:20 +0700

    add Payment,Concellation,SkinTest,SkinTestResult

--- src/main/java/nhom_java/skincarebookingsystem/models/Feedback.java
+++ src/main/java/nhom_java/skincarebookingsystem/models/Feedback.java
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-//package nhom_java.skincarebookingsystem.models;
-//import com.fasterxml.jackson.annotation.JsonFormat;
-//import jakarta.persistence.*;
-//import java.time.LocalDate;
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@PrimaryKeyJoinColumn(name = "customer_id")
-//public class Feedback extends Customer{
-//
-//    @Column(nullable = false)
-//    private String content;
-//    @Column(nullable = false, updatable = false)
-//    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
-//    private LocalDateTime feedbackDate;
-//    public Feedback() {
-//        this.feedbackDate = LocalDateTime.now();
-//    }
-//
-//    public String getContent() {
-//        return content;
-//    }
-//
-//    public void setContent(String content) {
-//        this.content = content;
-//    }
-//
-//    public LocalDateTime getFeedbackDate() {
-//        return feedbackDate;
-//    }
-//
-//    public void setFeedbackDate(LocalDateTime feedbackDate) {
-//        this.feedbackDate = feedbackDate;
-//    }
-//}
-=======
 package nhom_java.skincarebookingsystem.models;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
@@ -81,4 +44,3 @@ public class Feedback extends Customer{
         this.feedbackDate = feedbackDate;
     }
 }
->>>>>>> 19b7972c9144d513f357e9dbd0f7d06b7776f810
--- src/main/java/nhom_java/skincarebookingsystem/repositories/FeedbackRepository.java
+++ src/main/java/nhom_java/skincarebookingsystem/repositories/FeedbackRepository.java
@@ -1,11 +1,11 @@
-//package nhom_java.skincarebookingsystem.repositories;
-//
-//import nhom_java.skincarebookingsystem.models.Feedback;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//import java.util.*;
-//@Repository
-//public interface FeedbackRepository extends JpaRepository<Feedback, Long>{
-//    Optional<Feedback> findByEmail(String email);
-//    void deleteByEmail(String email);
-//}
+package nhom_java.skincarebookingsystem.repositories;
+
+import nhom_java.skincarebookingsystem.models.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long>{
+    Optional<Feedback> findByEmail(String email);
+    void deleteByEmail(String email);
+}
