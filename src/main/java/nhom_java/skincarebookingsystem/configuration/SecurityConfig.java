c1bbbcd00f46e2a66ac517625e0b306d8d37c6bd
vantien68 <tienlv8664@ut.edu.vn>
2025-03-29 23:21:49 +0700

    add Spring Security 1

--- src/main/java/nhom_java/skincarebookingsystem/configuration/SecurityConfig.java
+++ src/main/java/nhom_java/skincarebookingsystem/configuration/SecurityConfig.java
@@ -1,4 +1,44 @@
 package nhom_java.skincarebookingsystem.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.signerKey}")
+    private String signerKey;
+
+    private static final String[] PUBLIC_URLS = {
+            "/auth/token",
+            "/auth/introspect",
+            "/skin/cus"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.ignoringRequestMatchers(PUBLIC_URLS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PUBLIC_URLS).permitAll() // Không cần xác thực
+                        .anyRequest().authenticated() // Cần xác thực cho các API còn lại
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))) // Thêm xác thực JWT
+                .build();
+    }
+
 }
